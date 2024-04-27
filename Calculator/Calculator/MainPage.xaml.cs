b28654bf07d6667634e0c551b4af0fbda4467176
Lks <nbvf01@list.ru>
2024-04-27 03:48:17 +0300

    проектирование  зизайна при повороте экрана

--- Calculator/Calculator/MainPage.xaml.cs
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Globalization;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
@@ -27,25 +28,181 @@ namespace Calculator
                 _oldwidth = width;
                 _oldhight = height;
                 if (width>height)
-                {
-                    secondlabel.Text = "Landscape";
+                { // landshaft
+                    mainlabel.Margin = new Thickness(0, 10, 10, 0);
+                    secondlabel.Margin = new Thickness(0, 0, 0, 0);
+                    GridForGrids.Margin = new Thickness(0, 0, 0, 0);
+                    GridForGrids.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(4, GridUnitType.Star)}) ;
+                    Grid.SetColumn(MainGrid, 1);
+                    foreach (Button btn in MainGrid.Children)
+                    {
+                        btn.FontSize = 14;
+                        btn.FontAttributes = FontAttributes.Bold;
+                    }
+                    mainlabel.FontSize = 30;
+                    secondlabel.FontSize = 20;
+
+                    Grid Grid2 = new Grid();
+                    
+                    GridForGrids.Children.Add(Grid2);
+                     // GridForGrids.ColumnDefinitions.Contains(new ColumnDefinition { Width = new GridLength(7, GridUnitType.Star) });
+                    GridForGrids.ColumnDefinitions[0].Width = new GridLength(3,GridUnitType.Star);
+                    Grid.SetColumn(Grid2, 0);
+                    Grid2.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                    Grid2.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                    Grid2.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+                    Grid2.RowDefinitions.Add(new RowDefinition { Height =  new GridLength (1, GridUnitType.Star) });
+                    Grid2.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    Grid2.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    Grid2.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    Grid2.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    Grid2.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    
+                    // inv switch
+                    Label label = new Label();
+                    label.FontSize = 17;
+                    label.Text = "Inv";
+                    label.TextColor = Color.FromHex("#bdbdc7fc");
+                    label.FontAttributes= FontAttributes.Bold;
+                    label.Margin = new Thickness(0,6, 0, 0);
+
+                    Switch @switch = new Switch();
+                    @switch.Toggled += (sender, e) =>
+                    {
+                        inv = !inv;
+                    };
+
+                    StackLayout stackLayout = new StackLayout();
+                    stackLayout.Orientation = StackOrientation.Horizontal;
+                    stackLayout.Children.Add(@switch);
+                    stackLayout.Children.Add(label);
+                    stackLayout.BackgroundColor = Color.FromHex("#333233");
+
+                    Frame frame = new Frame();
+                    frame.Content = stackLayout;
+                    frame.CornerRadius = 30;
+                    frame.Padding = 0;
+                    Grid2.Children.Add(frame);
+                    Grid.SetColumn(frame, 0); 
+                    Grid.SetRow(frame, 5);
+                    // inv switch/>
+                    // deg 
+                    RadioButton deg =  new RadioButton ();
+                    deg.GroupName = "corner";
+                    deg.Content = "Deg";
+                    deg.BackgroundColor = Color.FromHex("#333233");
+                    deg.CornerRadius = 30;
+                    deg.FontSize = 15;
+                    deg.FontAttributes = FontAttributes.Bold;
+                    deg.TextColor = Color.FromHex("#bdbdc7fc");
+                    deg.CheckedChanged += (sender, e) =>
+                    {
+                        if (deg.IsChecked == true) { Deg = true; Rad = false; }
+                        else { Deg = false; }
+                    };
+                    Grid2.Children.Add(deg);
+                    Grid.SetColumn(deg, 1);
+                    Grid.SetRow(deg, 5);
+                    //  deg/>
+                    // rad  
+                    RadioButton rad = new RadioButton();
+                    rad.GroupName = "corner";
+                    rad.Content = "Rad";
+                    rad.BackgroundColor = Color.FromHex("#333233");
+                    rad.CornerRadius = 30;
+                    rad.FontSize = 15;
+                    rad.FontAttributes = FontAttributes.Bold;
+                    rad.TextColor = Color.FromHex("#bdbdc7fc");
+                    rad.CheckedChanged += (sender, e) =>
+                    {
+                        if (rad.IsChecked == true) { Rad = true; Deg = false; }
+                        else { Rad = false; }
+                    };
+                    Grid2.Children.Add(rad);
+                    Grid.SetColumn(rad, 2);
+                    Grid.SetRow(rad, 5);
+                    // rad/>
+                    int k = 0;
+                    for (int i = 0;i < 3; i++)
+                    {
+                        for (int j = 0;j < 5;j++)
+                        {
+                                addbutton(k.ToString(),true,j,i);
+                            k++;
+                        }
+                    }
+
+                    void addbutton(string text,bool ischar, int row,int column)
+                    {
+                        if (ischar) 
+                        {
+                            Button button = new Button();
+                            button.Text = text;
+                            button.Style = InfoStyle2;
+                            button.FontAttributes = FontAttributes.Bold;
+                            button.Clicked += charbuttonclick;
+                            Grid2.Children.Add(button);
+                            Grid.SetColumn(button, column);
+                            Grid.SetRow(button, row);
+                        }
+                        else
+                        {
+                            Button button = new Button();
+                            button.Text = text;
+                            button.Style = InfoStyle2;
+                            button.Clicked += numsbuttonclick;
+                            Grid2.Children.Add(button);
+                            Grid.SetColumn(button, column);
+                            Grid.SetRow(button, row);
+                        } 
+                            
+                    }
+
                 }
-                else { secondlabel.Text = "Portrait"; }
+                else { //  Portriet
+                    mainlabel.Margin = new Thickness(0, 50, 10, 0);
+                    secondlabel.Margin = new Thickness(0, 0, 20, 0);
+                    GridForGrids.Margin = new Thickness(0, 11, 0, 0);
+                    if (GridForGrids.ColumnDefinitions.Count > 1)
+                    {
+                        GridForGrids.Children.RemoveAt(1);
+
+                    }
+                    if (GridForGrids.ColumnDefinitions.Count > 1) 
+                    {
+
+                        GridForGrids.ColumnDefinitions.RemoveAt(1);
+                        Grid.SetColumn(MainGrid, 0);
+
+                    }
+                    foreach(Button btn in MainGrid.Children)
+                    {
+                        btn.FontSize =  40;
+                        btn.FontAttributes = FontAttributes.None;
+                    }
+                    mainlabel.FontSize = 40;
+                    secondlabel.FontSize= 30;
+                }
             }
             
         }
         
 
         List<double> numsarr = new List<double>();
-        List<string> chararr = new List<string>(); 
-        
+        List<string> chararr = new List<string>();
+        string[] enginerschars = { "x³", "e", "f", "t", "lnv", "sin", "cos", "tan", "log", "ln", "!", "deg" };
+
+        private bool Rad = false;
+        private bool Deg = true;
+        private bool inv = false;
         private bool haverror = false;
 
          public void numsbuttonclick(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
 
-            if (mainlabel.Text.EndsWith("%") | mainlabel.Text.EndsWith("²") )
+            if (mainlabel.Text.EndsWith("%") | mainlabel.Text.EndsWith("²") | mainlabel.Text.EndsWith("π"))
             {
                 return ;
             }
